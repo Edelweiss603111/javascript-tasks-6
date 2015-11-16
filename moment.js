79ab8e4ac906ec18ce243a1e7f2800e1ae1ac209
Edelweiss603111 <arinavaruhina@gmail.com>
2015-11-17 00:09:40 +0500

    Доделала

--- moment.js
+++ moment.js
@@ -10,11 +10,21 @@ module.exports = function () {
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var result = pattern.replace('%DD', this.date.day);
+            result = result.replace('%HH', setTime(this.date.hours + this.timezone));
+            return result.replace('%MM', setTime(this.date.minutes));
         },
-
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            console.log(moment);
         }
     };
 };
+
+function setTime(time) {
+    if (time < 10) {
+        return '0' + time;
+    }
+    return time;
+}
